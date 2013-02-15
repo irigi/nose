8b91fff63a9067c53148c3cf7aa3eb6bbc6407c2
irigi <irigi@matfyz.cz>
2013-02-15 22:26:26 +0100

    898 version run on the cluster

--- src/pert/modules/montecarlo/module_montecarlo.F90
+++ src/pert/modules/montecarlo/module_montecarlo.F90
@@ -53,7 +53,7 @@ module module_montecarlo
  	complex(dpc), dimension(:,:,:), allocatable, private 	:: rho, rho_coherent
  	complex(dpc), dimension(:,:,:,:), allocatable, private 	:: rho_micro
  	integer(i4b), dimension(:,:), allocatable, private		:: rho_micro_N
- 	real(dp), parameter, private :: CoherentFactorMultiplierToLowerJumpProb = 0.5_dp
+ 	real(dp), parameter, private :: CoherentFactorMultiplierToLowerJumpProb = 0.1_dp
 
  	integer(i1b), dimension(:,:,:), allocatable 	:: trajectory_depository
 ! 	integer(i4b), dimension(:,:), allocatable 	:: factor_depository
