5d68fb0a439debd47b2d2f3fcb32d376e9226fc1
irigi <irigi@matfyz.cz>
2013-03-12 19:55:51 +0100

    correction in output of coft

--- src/pert/modules/montecarlo/module_montecarlo.F90
+++ src/pert/modules/montecarlo/module_montecarlo.F90
@@ -2900,7 +2900,7 @@ module module_montecarlo
 
         j = 1
         do while (j <= size(all_hoft(i)%gg))
-            write(22,*) gt(1)*dt*(j-1),' ',real(all_goft(i)%gg(j)),' ',aimag(all_goft(i)%gg(j))
+            write(22,*) gt(1)*dt*(j-1),' ',real(all_hoft(i)%gg(j)),' ',aimag(all_hoft(i)%gg(j))
             j = j + 1
         end do
 
@@ -2913,7 +2913,7 @@ module module_montecarlo
 
         j = 1
         do while (j <= size(all_goft(i)%gg))
-            write(22,*) gt(1)*dt*(j-1),' ',real(all_goft(i)%gg(j)),' ',aimag(all_goft(i)%gg(j))
+            write(22,*) gt(1)*dt*(j-1),' ',real(all_coft(i)%gg(j)),' ',aimag(all_coft(i)%gg(j))
             j = j + 1
         end do
 
