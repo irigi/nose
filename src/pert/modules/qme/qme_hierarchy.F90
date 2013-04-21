1d37f039373d6c2569b31970f2e32b1fc84f44f7
irigi <irigi@matfyz.cz>
2013-04-21 02:32:18 +0200

    667 - relax

--- src/pert/modules/qme/qme_hierarchy.F90
+++ src/pert/modules/qme/qme_hierarchy.F90
@@ -1784,7 +1784,7 @@ module qme_hierarchy
       complex(dpc), intent(out) :: result(:,0:,0:)
       integer:: n,j, nnp
       complex(dpc), parameter   :: iconst = dcmplx(0.0, 1.0)
-      real(dp), parameter       :: RELAX = 0/100.0_dp
+      real(dp), parameter       :: RELAX = 1/100.0_dp
 
       result(:,:,:) = 0.0
 
